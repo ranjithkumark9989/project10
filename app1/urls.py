20f0190654511125d1ce0aa7de92f2c553712c46
ranjithkumark9989 <ranjithkumark9989@gmail.com>
2022-11-29 10:53:48 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app1.views import *
+app_name='ranjith'
+urlpatterns=[
+    path('a1_first/',a1_first,name='a1_first'),
+]
+
+urlpatterns=[
+    path('a1_second/',a1_second,name='a1_second'),
+]
